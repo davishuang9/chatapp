87e37cd07a01c56c7517fb57a92496b2ae2aee14
Davis Huang <davishuang9@gmail.com>
2020-02-19 10:32:11 -0800

    remove extraneous files, add js formatters

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,9 +1,8 @@
 import React from 'react';
 import { render } from '@testing-library/react';
-import App from './App';
+import App from 'src/App';
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test('app renders', () => {
+  const app = render(<App />);
+  expect(app.baseElement.nodeName).toEqual('BODY');
 });
